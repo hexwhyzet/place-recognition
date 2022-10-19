d01d1bd679cee94637dd30412d388462ab2fc424
teddyzxcv <48234309+teddyzxcv@users.noreply.github.com>
2022-10-19 23:00:22 +0300

    first commit

---
+++ Place Recognition/Place Recognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Place Recognition
+//
+//  Created by ZhengWu Pan on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
