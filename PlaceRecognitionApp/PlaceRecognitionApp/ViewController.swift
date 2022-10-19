66263ba89ff45df09bc91002fc569f4513c14600
teddyzxcv <48234309+teddyzxcv@users.noreply.github.com>
2022-10-20 01:27:19 +0300

    Add repo and gitignore

---
+++ PlaceRecognitionApp/PlaceRecognitionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceRecognitionApp
+//
+//  Created by ZhengWu Pan on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
